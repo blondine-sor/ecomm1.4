4b1e7c493d0f3d387b971034fbf2893207668c43
blondine-sor <bkeumani@gmail.com>
2023-10-11 15:49:31 -0400

    Requete avec mysqli

--- index.php
+++ index.php
@@ -13,10 +13,47 @@ else{
     echo"Error : Not connected to the $db database";
 }
 
+//utilisation d'une requete sql dans php
+
+$result1 = mysqli_query($conn, " SELECT * FROM user WHERE id=2");
+
+$data1 = mysqli_fetch_row($result1);
+
+echo"</br>";
+echo "Premier fetch";
+echo"</br>";
+echo "</br>";
+var_dump($result1);
+echo"</br>";
+echo"</br>";
+var_dump($data1);
+
+$result2 = mysqli_query($conn, " SELECT * FROM user WHERE id=1");
+
+$data2 = mysqli_fetch_assoc($result2);
+
+echo"</br>";
+echo "Second fetch";
+echo"</br>";
+echo "</br>";
+var_dump($result2);
+echo"</br>";
+echo"</br>";
+var_dump($data2);
 
 
+$result3 = mysqli_query($conn, " SELECT user_name,email, id FROM user WHERE id=1");
 
+$data3 = mysqli_fetch_assoc($result3);
 
+echo"</br>";
+echo "Troisieme fetch";
+echo"</br>";
+echo "</br>";
+var_dump($result3);
+echo"</br>";
+echo"</br>";
+var_dump($data3);
 
 
 
